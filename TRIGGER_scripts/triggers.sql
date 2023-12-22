2aaabf7d0c4422e686d4a613f7eb9d20b4085657
Sasha <amosin2002@mail.ru>
2023-12-22 18:14:15 +0300

    Fixes

--- TRIGGER_scripts/triggers.sql
+++ TRIGGER_scripts/triggers.sql
@@ -1,49 +1,45 @@
 set search_path = company_database, public;
 
-CREATE OR REPLACE FUNCTION prevent_concurrent_booking()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM company_database.room_booking rb
-        WHERE rb.room_id = NEW.room_id
-          AND rb.booking_date = NEW.booking_date
-          AND (
-            (NEW.start_booking BETWEEN rb.start_booking AND rb.end_booking)
-            OR (NEW.end_booking BETWEEN rb.start_booking AND rb.end_booking)
-            OR (NEW.start_booking <= rb.start_booking AND NEW.end_booking >= rb.end_booking)
+create or replace function prevent_concurrent_booking()
+returns trigger as $$
+begin
+    if exists (
+        select 1
+        from company_database.room_booking rb
+        where rb.room_id = new.room_id
+          and rb.booking_date = new.booking_date
+          and (
+            (new.start_booking between rb.start_booking and rb.end_booking)
+            or (new.end_booking between rb.start_booking and rb.end_booking)
+            or (new.start_booking <= rb.start_booking and new.end_booking >= rb.end_booking)
           )
-    ) THEN
-        RAISE EXCEPTION 'Concurrent booking is not allowed for room % and date %',
-            NEW.room_id, NEW.booking_date;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+    ) then
+        raise exception 'Concurrent booking is not allowed for room % and date %',
+            new.room_id, new.booking_date;
+    end if;
+    return new;
+end;
+$$ language plpgsql;
 
-CREATE TRIGGER prevent_concurrent_booking_trigger
-BEFORE INSERT OR UPDATE
-ON company_database.room_booking
-FOR EACH ROW
-EXECUTE FUNCTION prevent_concurrent_booking();
+create trigger prevent_concurrent_booking_trigger
+before insert or update
+on company_database.room_booking
+for each row
+execute function prevent_concurrent_booking();
 
-CREATE TRIGGER after_insert_employee_trigger
-AFTER INSERT ON company_database.employee
-FOR EACH ROW EXECUTE FUNCTION after_insert_employee();
+create or replace function before_update_employee_is_active()
+returns trigger as $$
+begin
+    if new.is_active is distinct from old.is_active and new.is_active = false then
+        update pass
+        set is_valid = false
+        where employee_id = old.employee_id;
+    end if;
+    return new;
+end;
+$$ language plpgsql;
 
-CREATE OR REPLACE FUNCTION before_update_employee_is_active()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF NEW.is_active IS DISTINCT FROM OLD.is_active AND NEW.is_active = false THEN
-        UPDATE pass
-        SET is_valid = false
-        WHERE employee_id = OLD.employee_id;
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER before_update_employee_is_active_trigger
-BEFORE UPDATE ON employee
-FOR EACH ROW EXECUTE FUNCTION before_update_employee_is_active();
+create trigger before_update_employee_is_active_trigger
+before update on company_database.employee
+for each row
+execute function before_update_employee_is_active();
