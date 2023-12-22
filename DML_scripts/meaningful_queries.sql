7d734e664e77ba28321d45d474b9f5a6fbbedf99
Alexander Vinogradov <alexvingra@mail.ru>
2023-12-22 11:36:12 +0300

    Add new insertions and fixes

--- DML_scripts/meaningful_queries.sql
+++ DML_scripts/meaningful_queries.sql
@@ -6,7 +6,7 @@ set search_path = company_database, public;
 select mr.room_id, mr.name
 from meeting_room mr
          inner join office o on mr.office_code = o.office_code
-where o.name = 'Branch Office 2'
+where o.name = 'Times Square Office'
 
 except
 
@@ -14,16 +14,16 @@ select mr.room_id, mr.name
 from meeting_room mr
          inner join room_booking rb
                     on mr.room_id = rb.room_id
-where ('2023-01-10 15:00'::timestamp < rb.end_booking and rb.end_booking <= '2023-01-10 16:00'::timestamp)
-   or ('2023-01-10 15:00'::timestamp <= rb.start_booking and rb.start_booking < '2023-01-10 16:00'::timestamp)
-   or (rb.start_booking <= '2023-01-10 15:00'::timestamp and '2023-01-10 16:00'::timestamp <= rb.end_booking);
+where ('2023-12-21 13:00'::timestamp < rb.end_booking and rb.end_booking <= '2023-12-21 14:00'::timestamp)
+   or ('2023-12-21 13:00'::timestamp <= rb.start_booking and rb.start_booking < '2023-12-21 14:00'::timestamp)
+   or (rb.start_booking <= '2023-12-21 13:00'::timestamp and '2023-12-21 14:00'::timestamp <= rb.end_booking);
 
 ----------------------------------------------------------------------------------------
 
 -- Посчитать по каждому офису кол-во человек в указанную дату (считаем, что офисы не работают по ночам)
 -- Ожидаемый результат:  office_name ,кол-во человек
 
-select ocpi.office_code, coalesce(count(distinct ocpi.time), 0)
+select ocpi.office_code, coalesce(count(distinct ocpi.time), 0) as total_employees
 from (select o.office_code,
              et.pass_id,
              case
